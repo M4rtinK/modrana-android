40d9c4e7ba37b4c6e0287c71874d7a93e69a1c04
Martin Kolman <martin.kolman@gmail.com>
2014-12-25 17:48:39 +0100

    Repurpose the gPodder Android project for modRana

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <QGuiApplication>
 
 #include <QQuickView>
+#include <QQmlApplicationEngine>
+#include <QQmlEngine>
 #include <QFontDatabase>
 
 #include <QStandardPaths>
@@ -10,18 +12,32 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    //setenv("QML_IMPORT_TRACE", "1", 1);
+    setenv("PYTHONVERBOSE", "2", 2);
+    //setenv("PYTHONVERBOSE", "2", 1);
+
+
     // gPodder storage directory
-    QString data(QDir(QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation)).filePath("gPodder"));
-    qputenv("GPODDER_HOME", data.toUtf8());
+    //QString data(QDir(QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation)).filePath("gPodder"));
+    //qputenv("GPODDER_HOME", data.toUtf8());
 
     // gPodder QML UI fonts and themeing
-    QFontDatabase::addApplicationFont(":/touch/icons/iconic_fill.ttf");
-    QFontDatabase::addApplicationFont(":/touch/fonts/source-sans-pro.extralight.ttf");
+    //QFontDatabase::addApplicationFont(":/touch/icons/iconic_fill.ttf");
+    //QFontDatabase::addApplicationFont(":/touch/fonts/source-sans-pro.extralight.ttf");
 
-    QQuickView view;
-    view.setSource(QUrl("qrc:/touch/gpodder.qml"));
-    view.setResizeMode(QQuickView::SizeRootObjectToView);
-    view.show();
+    //QQmlEngine engine;
+    //engine.addImportPath(":/modrana/modules/gui_modules/gui_qt5/qml/universal_components/controls/");
+    //engine.addImportPath("qrc:/modrana/modules/gui_modules/gui_qt5/qml/universal_components/controls/");
+
+    //QQuickView view;
+    //view.engine()->addImportPath(":/modules/gui_modules/gui_qt5/qml/universal_components/controls/");
+    //view.engine()->addImportPath("qrc:/modules/gui_modules/gui_qt5/qml/universal_components/controls/");
+    QQmlApplicationEngine engine;
+    engine.addImportPath(":/modules/gui_modules/gui_qt5/qml/universal_components/controls/");
+    engine.load(QUrl("qrc:/modules/gui_modules/gui_qt5/qml/main.qml"));
+    //view.setSource(QUrl("qrc:/modules/gui_modules/gui_qt5/qml/main.qml"));
+    //view.setResizeMode(QQuickView::SizeRootObjectToView);
+    //view.show();
 
     return app.exec();
 }

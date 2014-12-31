a561f11f1b159e7d99d9f38889f47a16e8a0b317
Martin Kolman <martin.kolman@gmail.com>
2014-12-31 19:38:20 +0100

    Add missing apk build steps
    Looks like some build steps, like actually building the "launcher"
    source code, were left of the script and have now been added. :)
    
    Also it turns out that for the build to finish successfully,
    paths to the Android NDK & SDK and to Qt Android qmake need to be
    provided, so and the script now expects the script "env.sh" to exist in the
    scripts folder, which exports the needed paths as environmental
    variables.
    
    So like this, it is now possible to create a new modRana apk directly
    from command line in a rather automatic fashion. :)

--- scripts/create_apk.sh
+++ scripts/create_apk.sh
@@ -8,6 +8,20 @@
 # make sure the PWD is consistent even if the script is called
 # from a different directory
 cd "$(dirname "$0")"
+# source the needed environmental variables
+
+if [ ! -f "env.sh" ]
+then
+        echo "Error: Environment definition file is missing!"
+        echo "You need to create an env.sh file in the scripts folder,"
+        echo "which exports the following environmental variables:"
+        echo "* ANDROID_NDK_ROOT - path to the root of an Android NDK"
+        echo "* ANDROID_SDK_ROOT - path to the root of an Android SDK"
+        echo "* ANDROID_QMAKE - path to qmake executable from the Qt Android SDK"
+        exit 1
+fi
+
+source env.sh
 
 APK_PATH=android-build/bin/QtApp-debug.apk
 OUTPUT_FOLDER=apks
@@ -16,10 +30,23 @@ CURRENT_VERSION=`grep versionCode ../android/AndroidManifest.xml | sed -e 's/.*v
 cd ..
 
 # generate the qrc manifest
+echo "* cleaning *"
+make clean
+echo "* generating qrc manifest *"
 ./qrcgen.py modrana "/"
+echo "* running qmake *"
+$ANDROID_QMAKE modrana-android.pro -r -spec android-g++ CONFIG+=debug
+echo "* running make *"
+make
+echo "* removing old android build dir *"
+rm -rf android-build
+echo "* running make install *"
+make INSTALL_ROOT=android-build install
 
+echo "* running creating apk *"
 androiddeployqt --deployment bundled --android-platform android-14 --output android-build
 
+echo "* moving resulting APKs to the apks folder *"
 cp $APK_PATH ${OUTPUT_FOLDER}/modrana-latest.apk
 mv $APK_PATH ${OUTPUT_FOLDER}/modrana-${CURRENT_VERSION}.apk
 
